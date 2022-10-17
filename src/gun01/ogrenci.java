0cf9b73b31ad3693351f39553ad1e63bc86ca983
alkan <alkannarin1986@gmail.com>
2022-10-17 11:19:30 +0300

    merhaba 4

--- src/gun01/ogrenci.java
+++ src/gun01/ogrenci.java
@@ -5,5 +5,6 @@ public class ogrenci {
         System.out.println("merhaba 1");
         System.out.println("merhaba 2");
         System.out.println("merhaba 3");
+        System.out.println("merhaba 4");
     }
 }
